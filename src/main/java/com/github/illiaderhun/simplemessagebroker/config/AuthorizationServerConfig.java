4d1198124ef72dadcb1514defca1450b29da9dd3
Illia Derhun <illya.dergun@gmail.com>
2021-01-29 21:25:08 +0200

    Created first time login page

--- src/main/java/com/github/illiaderhun/simplemessagebroker/config/AuthorizationServerConfig.java
+++ src/main/java/com/github/illiaderhun/simplemessagebroker/config/AuthorizationServerConfig.java
@@ -34,10 +34,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory().
-                withClient("my-trusted-client")
+                withClient("smbroot")
                 .authorizedGrantTypes("client_credentials", "password")
                 .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT").scopes("read", "write", "trust")
-                .resourceIds("oauth2-resource").accessTokenValiditySeconds(5000).secret("secret");
+                .resourceIds("oauth2-resource").accessTokenValiditySeconds(5000).secret("toor");
     }
 
 
